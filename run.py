449a11e8b724309b8f56f594b2a1c0dc2c00b9d7
Achrafness <achraf.nessighaoui13@gmail.com>
2025-12-01 20:51:19 +0100

    Disable Flask reloader to fix volume mount issues

--- run.py
+++ run.py
@@ -21,4 +21,6 @@ def make_shell_context():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    debug = os.environ.get('FLASK_ENV', 'production') == 'development'
+    app.run(debug=debug, host='0.0.0.0', port=5000, use_reloader=False)
